e186c10e5fc1f1248d3fdf39d327643379c6634b
Stuart Longland <me@vk4msl.id.au>
2018-04-08 16:56:39 +1000

    cache: Add cache abstract class.
    Behaves like a forgetful mapping object.

---
+++ tornado_gallery/cache.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+from time import time
+from collections import Mapping
+
+
+class Cache(object):
+    """
+    A key-value cache object that expires entries after a period.
+    """
+
+    def __init__(self, cache_duration=300.0):
+        self._cache_duration = float(cache_duration)
+        self._items = {}
+
+    def __getitem__(self, key):
+        """
+        Retrieve the content of the given key.
+        """
+        try:
+            (_, value) = self._items[key]
+        except KeyError:
+            value = self._fetch(key)
+        self._items[key] = (time() + self._cache_duration, value)
+        return value
+
+    def __iter__(self):
+        now = time.time()
+        for key, (ex, _) in self._items.item():
+            if ex > now:
+                yield key
+
+    def __len__(self):
+        now = time.time()
+        return len(list(filter(
+            lambda v : v[0] > now, self._items.values())))
+
+    def purge(self):
+        """
+        Purge the cache of expired content.
+        """
+        for key, (expiry, _) in list(self._items.items()):
+            if expiry < time():
+                self._items.pop(key, None)
+
+    @property
+    def next_expiry(self):
+        """
+        Return the time of the next expiry, if any.
+        """
+        expiries = [ex for (ex, _) in self._items.values()]
+        if len(expiries):
+            return min(expiries)
+        return None
