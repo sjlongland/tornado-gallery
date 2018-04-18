5a6349a19309ff9a7e8a331454f5c400547a3e86
Stuart Longland <me@vk4msl.id.au>
2018-04-18 14:38:24 +1000

    Add distribution files.

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+from setuptools import setup
+from tornado_gallery import __version__
+
+setup (name='tornado_gallery',
+        version=__version__,
+        install_requires = [
+            'tornado',
+            'pillow',
+            'filemagic',
+        ],
+        entry_points = {
+            'console_scripts': [
+                'tornado-gallery=tornado_gallery.server:main',
+            ]
+        },
+	packages = [
+            'tornado_gallery',
+            'tornado_gallery.static',
+        ],
+)
---
+++ tornado_gallery/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# vim: set ts=4 sts=4 et tw=78 sw=4 si:
+
+__author__ = 'Stuart Longland'
+__copyright__ = 'Copyright 2018, Stuart Longland'
+__credits__ = ['Stuart Longland']
+__license__ = 'BSD'
+__version__ = '0.0.1'
+__maintainer__ = 'Stuart Longland'
+__email__ = 'me@vk4msl.id.au'
